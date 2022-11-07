24c3fbfb9ceec56681622fb7dbd286213516ca33
Gustuckit <117044606+crinnsh@users.noreply.github.com>
2022-11-07 23:19:19 +0100

    Initial commit.
    Committing original files.

---
+++ script.js
@@ -0,0 +1,150 @@
+/* GOOGLE */
+
+const query = document.getElementById("query");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.onclick = function() {
+    let url = "https://www.google.com/search?q=" + query.value;
+    window.open(url);
+}
+
+document.querySelector(".search-barGoogle").addEventListener("keyup", function() {
+    if(event.key == "Enter") {
+        searchBtn.onclick();
+    }
+})
+
+/* END */
+
+                                        /* ----- */
+
+/* WEATHER */
+
+window.onload = function() {
+    weather.search();
+};
+
+let weather = {
+    "apiKey": "c5e1c01ad608f6a0ea9e64ee01591ec4",
+
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apiKey
+        )
+            .then((response) => response.json())
+
+            .then((data) => this.displayWeather(data))
+    },
+
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+
+        console.log(name, icon, description, temp, humidity, speed);
+
+        document.querySelector(".city").innerText = "Weather in " + name;
+
+        /* document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +"@2x.png"; */
+
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon +".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind: " + speed + " m/s";
+    },
+
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function() {
+        weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function() {
+    if(event.key == "Enter") {
+        weather.search();
+    }
+})
+
+/* END */
+
+                                        /* ----- */
+
+/* TODO */
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+    todos.forEach((todo) => {
+        addTodo(todo);
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    addTodo();
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if (todo) {
+        todoText = todo.text;
+    }
+
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
+
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+
+            updateLS();
+        });
+
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+
+            todoEl.remove();
+
+            updateLS();
+        });
+
+        todosUL.appendChild(todoEl);
+
+        input.value = "";
+
+        updateLS();
+    }
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll("li");
+    
+    const todos = [];
+
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed"),
+        });
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
