cd9a3f389c877d8643b5884c1f44fd59c648362c
crinnsh <117044606+crinnsh@users.noreply.github.com>
2022-11-18 18:09:22 +0100

    added digital clock.
    digital clock was added at top of screen.

--- script.js
+++ script.js
@@ -148,3 +148,21 @@ function updateLS() {
 
     localStorage.setItem("todos", JSON.stringify(todos));
 }
+
+/* END */
+
+                                        /* ----- */
+
+/* CLOCK */
+
+function displayTime() {
+    var dateTime = new Date();
+    var hrs = dateTime.getHours();
+    var min = dateTime.getMinutes();
+    var sec = dateTime.getSeconds();
+
+    document.getElementById("hours").innerHTML = hrs;
+    document.getElementById("minutes").innerHTML = min;
+    document.getElementById("seconds").innerHTML = sec;
+}
+setInterval(displayTime, 10);
